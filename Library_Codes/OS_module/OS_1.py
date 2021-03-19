584a2c9b926701095f198c77b5f3f6555ebbe001
CODEST <79630363+Codest4221@users.noreply.github.com>
2021-03-19 15:27:30 +0300

    Create OS_1.py

---
+++ Library_Codes/OS_module/OS_1.py
@@ -0,0 +1,13 @@
+import os 
+# to get current directory 
+current_directory = os.getcwd()
+#create current directroy 
+created_directory = os.mkdir("C:\PYthonsil")
+# to change current directory 
+change = os.chdir("C:\program files")
+# to remove directory 
+os.rmdir("C:\PYthonsil") 
+#to return all stuff in directory 
+os.listdir("C:")
+# to return a lot of information about directory 
+root ,dirs,files in os.walk("C:")
