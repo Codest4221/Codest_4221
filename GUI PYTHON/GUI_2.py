a82566bc4874c2ed1cbb0bf3e2477a04a0aaa2d3
CODEST <79630363+Codest4221@users.noreply.github.com>
2021-03-26 00:58:18 +0300

    Create GUI_2.py

---
+++ GUI PYTHON/GUI_2.py
@@ -0,0 +1,21 @@
+from tkinter import*
+
+
+root = Tk()
+#Creating
+mylabel = Label(root,text = "Hello World" )
+mylabel_1 = Label(root,text = "My name is mete")
+
+#showing
+mylabel.grid(row = 0, column = 0)
+mylabel_1.grid(row = 1, column = 0)
+
+
+
+
+
+root.mainloop()
+
+
+
+# --Mete Bingöl--
