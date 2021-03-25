3eef94a244a2a205b8422b287bc8d6cecc00da9e
CODEST <79630363+Codest4221@users.noreply.github.com>
2021-03-26 00:57:28 +0300

    Create GUI_1.py

---
+++ GUI PYTHON/GUI_1.py
@@ -0,0 +1,16 @@
+from tkinter import*
+
+root = Tk()
+
+
+#Creating a label 
+
+mylabel = Label(root, text="Hello World")
+#showing it onto the screen 
+mylabel.pack()
+
+root.mainloop()
+
+
+
+# -- Mete Bingöl--
