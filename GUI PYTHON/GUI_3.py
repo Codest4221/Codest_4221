ae6b7ee6fd3300e3deafcde956a77491ffb81ba6
CODEST <79630363+Codest4221@users.noreply.github.com>
2021-03-27 00:42:12 +0300

    Create GUI_3.py

---
+++ GUI PYTHON/GUI_3.py
@@ -0,0 +1,16 @@
+from tkinter import*
+def myClick():
+    myLabel = Label(root, text = "look you clicked!!!")
+    myLabel.pack()
+
+
+
+root = Tk()
+
+#Create button  
+myButton = Button(root, text = "Click me", padx= 50 ,pady= 50,command = myClick,fg = "blue",bg="red") 
+myButton.pack()
+
+
+
+root.mainloop()
