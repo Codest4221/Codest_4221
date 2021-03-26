bac3becf2ffbb086302dbcdfe40572bf6dcc7bba
CODEST <79630363+Codest4221@users.noreply.github.com>
2021-03-27 00:54:30 +0300

    Create GUI_4.py

---
+++ GUI PYTHON/GUI_4.py
@@ -0,0 +1,22 @@
+from tkinter import*
+
+
+root = Tk()
+
+
+e = Entry(root, width= 50 ,fg = "red",bg = "black",borderwidth = 8)
+
+
+e.pack()
+e.insert(0 , "Give your Name")
+
+
+
+
+
+
+
+root.mainloop()
+
+
+
